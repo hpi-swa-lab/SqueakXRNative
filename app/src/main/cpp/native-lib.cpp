159a93641aa553f5b35f05ab05c5c337136eb5c9
leogeier <contact@leonardgeier.com>
2025-02-18 17:46:01 +0100

    file copying, launch squeak

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,56 @@
 #include <jni.h>
 #include <string>
+#include <stdio.h>
+#include <unistd.h>
+#include <android/log.h>
+#include <pthread.h>
+
+int fd[2];
+pthread_t thr;
+
+// https://codelab.wordpress.com/2014/11/03/how-to-use-standard-output-streams-for-logging-in-android-apps/
+static void *thread_func(void*)
+{
+    ssize_t rdsz;
+    char buf[128];
+    __android_log_write(ANDROID_LOG_DEBUG, ".squeakxrnative", "Waiting for content...");
+    while((rdsz = read(fd[0], buf, sizeof buf - 1))) {
+        if (rdsz <= 0) continue;
+        if(buf[rdsz - 1] == '\n') --rdsz;
+        buf[rdsz] = 0;  /* add null-terminator */
+        __android_log_print(ANDROID_LOG_DEBUG, ".squeakxrnative", "STDOUT: %s", buf);
+    }
+    return 0;
+}
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_swalab_squeakxrnative_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    setvbuf(stdout, 0, _IOLBF, 0);
+    setvbuf(stderr, 0, _IONBF, 0);
+    pipe(fd);
+    dup2(fd[1], 1);
+    dup2(fd[1], 2);
+
+    std::string hello = "Hello from C+++++++++";
+    if(pthread_create(&thr, 0, thread_func, 0) != 0) {
+        __android_log_write(ANDROID_LOG_ERROR, ".squeakxrnative", "Failed to create thread");
+    } else {
+        pthread_detach(thr);
+        __android_log_write(ANDROID_LOG_DEBUG, ".squeakxrnative", "Created thread");
+    }
+//    __android_log_write(ANDROID_LOG_VERBOSE, ".squeakxrnative", "========================================test");
     return env->NewStringUTF(hello.c_str());
+    printf("============ HELLOOOO WORLLDLDLDL ========");
+}
+
+extern "C" int run_squeak(int argc, char **argv, char **envp);
+
+extern "C" JNIEXPORT void JNICALL Java_com_swalab_squeakxrnative_MainActivity_printSomething() {
+    __android_log_write(ANDROID_LOG_DEBUG, ".squeakxrnative", "button c");
+    printf("============ HELLOOOO WORLLDLDLDL ========");
+    char *argv[1] = {"squeak"};
+    char *envp[1]= {nullptr};
+    run_squeak(1, argv, envp);
 }
--- app/src/main/java/com/swalab/squeakxrnative/MainActivity.kt
+++ app/src/main/java/com/swalab/squeakxrnative/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.swalab.squeakxrnative
 
+import android.content.res.AssetManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.widget.TextView
 import com.swalab.squeakxrnative.databinding.ActivityMainBinding
+import java.io.File
+import java.io.FileOutputStream
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,6 +21,48 @@ class MainActivity : AppCompatActivity() {
 
         // Example of a call to a native method
         binding.sampleText.text = stringFromJNI()
+
+        binding.button.setOnClickListener {
+            println("Button pressed")
+            printSomething()
+        }
+
+        updateImageInfo()
+
+        binding.buttonResetImage.setOnClickListener {
+            for (filename in squeakImageFiles) {
+                val inStream = assets.open(filename, AssetManager.ACCESS_BUFFER)
+                val externalFile = File(getExternalFilesDir(null)!!, filename)
+                println("Copying $filename to ${externalFile.absolutePath}")
+                val outStream = FileOutputStream(externalFile)
+                inStream.copyTo(outStream)
+                inStream.close()
+                outStream.close()
+            }
+        }
+    }
+
+    val squeakImageFiles = arrayOf("Squeak6.0-22148-64bit.changes", "Squeak6.0-22148-64bit.image", "SqueakV60.sources")
+
+    private fun updateImageInfo() {
+        var externalStorageDesc = "not readable or writable"
+        if (Environment.getExternalStorageState() == Environment.MEDIA_MOUNTED) {
+            externalStorageDesc = "writeable"
+        } else if (Environment.getExternalStorageState() == Environment.MEDIA_MOUNTED_READ_ONLY) {
+            externalStorageDesc = "read only"
+        }
+
+        val externalFilesDir = getExternalFilesDir(null)
+        val externalFiles = externalFilesDir?.list() ?: emptyArray()
+
+        val assetFiles: List<String> = assets.list("")?.map { file -> file.toString() } ?: emptyList<String>()
+        val fileStatus = squeakImageFiles.joinToString("\n" ) { file ->
+            val presentInAssetManager = "${if (file in assetFiles) "" else "not"} present in AssetManager"
+            val presentInExternalStorage = "${if (file in externalFiles) "" else "not"} present in ExternalStorage"
+            "\t$file\n\t\t$presentInAssetManager\n\t\t$presentInExternalStorage"
+        }
+
+        binding.textImageStatus.text = "External storage: $externalStorageDesc\nExternal storage dir: $externalFilesDir\nFile status:\n$fileStatus"
     }
 
     /**
@@ -24,6 +70,7 @@ class MainActivity : AppCompatActivity() {
      * which is packaged with this application.
      */
     external fun stringFromJNI(): String
+    external fun printSomething()
 
     companion object {
         // Used to load the 'squeakxrnative' library on application startup.
